b3ebe292804febdd16e760b468fec2f4fb2ac881
Montilla84 <montilla84@gmail.com>
2024-07-22 00:49:33 +0200

    chore: Add Vite configuration and TypeScript files

---
+++ my-app/src/pages/AboutUs.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const AboutUs: React.FC = () => {
+  return (
+    <div>
+      <Typography variant="h2" gutterBottom>
+        About Us
+      </Typography>
+      <Typography variant="body1">
+        Hola, soy la página de About Us.
+      </Typography>
+    </div>
+  );
+}
+
+export default AboutUs;
